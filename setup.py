2a353e64d542383f9dc79a92bada24e5aae65ff6
Clovis Gladstone <clovisgladstone@gmail.com>
2022-08-24 15:30:53 -0500

    minor fixes

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from setuptools import setup
 
 setup(
     name="text_preprocessing",
-    version="0.8.4",
+    version="1.0",
     author="The ARTFL Project",
     author_email="clovisgladstone@gmail.com",
     packages=["text_preprocessing", "text_preprocessing.lang"],
-    install_requires=["unidecode", "PyStemmer", "spacy>=3.0", "python-rapidjson", "mmh3", "requests", "lz4"],
+    install_requires=["unidecode", "PyStemmer", "spacy>=3.4", "orjson", "mmh3", "requests", "lz4"],
 )

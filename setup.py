db335b8d65de6832eacd28b5d533a30f7a259c08
Clovis Gladstone <clovisgladstone@gmail.com>
2024-05-10 09:21:06 -0500

    add missing dependency

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     author="The ARTFL Project",
     author_email="clovisgladstone@gmail.com",
     packages=["text_preprocessing", "text_preprocessing.lang"],
-    install_requires=["unidecode", "PyStemmer", "spacy>=3.7,<3.8", "orjson", "requests", "lz4", "regex"],
+    install_requires=[
+        "unidecode",
+        "PyStemmer",
+        "spacy>=3.7,<3.8",
+        "orjson",
+        "requests",
+        "lz4",
+        "regex",
+        "multiprocess",
+    ],
 )
